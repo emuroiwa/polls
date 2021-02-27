bb36d83c911329209f3ef56c4d8322d007f7e196
Ernest Muroiwa <emuroiwa@gmail.com>
2021-02-27 08:06:48 +0200

    admin header

--- pollster/polls/admin.py
+++ pollster/polls/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from polls.models import Choice, Question
 
+admin.site.site_header = "Pollster Admin"
+admin.site.index_title = "Pollster Admin"
+admin.site.site_title = "Pollster Admin"
+
 admin.site.register(Question)
 admin.site.register(Choice)
