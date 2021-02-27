4e8dd7e7b1095150bd179ce88ea02a9a3271ea27
Ernest Muroiwa <emuroiwa@gmail.com>
2021-02-27 09:11:07 +0200

    index and detail methods

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 from .models import Question, Choice
 
 # get question and display
-def index(request)
+def index(request):
+    latest_question_list = Question.objects.all
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+        context = {'question': question}
+        return render(request, 'polls/detail.html', context)
+    except print(0):
+        pass
 
 # Create your views here.
